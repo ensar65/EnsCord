023964aaea52b98f38b1c09158af4f9874219060
Ensar Işık <devensar0@gmail.com>
2023-06-10 00:32:47 +0300

    Command handler

--- src/events/interactionCreate/src/index.ts
+++ src/events/interactionCreate/src/index.ts
@@ -4,6 +4,12 @@ import {Interaction} from "discord.js";
 
 export default class Ready extends Event implements EventInterface {
     onRun(interaction: Interaction): void {
-
+        if(interaction.isChatInputCommand()) {
+            let commandName = interaction.commandName;
+            if(commands.has(commandName)) {
+                let command = commands.get(commandName);
+                command?.onCommandSender(interaction)
+            } else  interaction.reply("Command not found.")
+        }
     }
 }
