862786cafabf41d3ee79dfb77cc15aa231f08075
Ensar Işık <devensar0@gmail.com>
2023-06-10 00:17:14 +0300

    syntax

--- src/commands/ping/config.ts
+++ src/commands/ping/config.ts
@@ -2,7 +2,7 @@ import Command from  "../../libs/interfaces/configs/Command"
 import {SlashCommandBuilder} from "discord.js";
 export default function () {
     return <Command> {
-        "command" : new SlashCommandBuilder().setName("ping").setDescription("Shows the bots ping").toJSON(),
+        "command" : new SlashCommandBuilder().setName("ping").setDescription("Shows the bots ping"),
         "commandType" : "client",
         "main" : "index"
     }
