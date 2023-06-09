dd03f58ba596ea3b022f0b9c075d2f8e40c98058
Ensar Işık <devensar0@gmail.com>
2023-06-10 00:32:12 +0300

    implement

--- src/commands/ping/src/index.ts
+++ src/commands/ping/src/index.ts
@@ -1,5 +1,10 @@
 import Command from "../../Command";
+import CommandInterface from "../../../libs/interfaces/CommandInterface";
+import {Interaction} from "discord.js";
 
-export default class PingCommand extends Command {
+export default class PingCommand extends Command implements CommandInterface {
+    onCommandSender(interaction: Interaction): void {
+
+    }
 
 }
