704b58306a04ad0ae6c7e4bce16f7027ce46d935
Ensar Işık <devensar0@gmail.com>
2023-06-11 00:46:20 +0300

    added slash_command global variable

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,14 @@
-import {Collection} from "discord.js";
+import {Collection, SlashCommandBuilder} from "discord.js";
 import CommandInterface from "./src/libs/interfaces/CommandInterface";
 
 declare module NodeJS {
     interface Global {
-        commands : Collection<string, CommandInterface>
+        commands: Collection<string, CommandInterface>
+        slash_commands: Collection<any, SlashCommandBuilder>
     }
 }
 
 declare global {
-    var commands : Collection<string, CommandInterface>
+    var commands: Collection<string, CommandInterface>
+    var slash_commands: Collection<any, SlashCommandBuilder>
 }
