5583be878a2af64248e6142abf2f7ab6e2c34c19
Ensar Işık <devensar0@gmail.com>
2023-06-11 00:46:33 +0300

    ping command

--- src/commands/ping/src/index.ts
+++ src/commands/ping/src/index.ts
@@ -1,10 +1,10 @@
 import Command from "../../Command";
 import CommandInterface from "../../../libs/interfaces/CommandInterface";
-import {Interaction} from "discord.js";
+import {BaseInteraction, CommandInteraction, Interaction} from "discord.js";
 
 export default class PingCommand extends Command implements CommandInterface {
-    onCommandSender(interaction: Interaction): void {
-
+    onCommandSender(interaction: CommandInteraction): void {
+        interaction.reply("My ping : " + interaction.client.ws.ping)
     }
 
 }
