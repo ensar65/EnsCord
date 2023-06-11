bcfb965cabf0b4253acfe437f0e21dae172f3fbf
Ensar Işık <devensar0@gmail.com>
2023-06-11 12:42:59 +0300

    some console changes

--- src/libs/app/slashCommandPusher.ts
+++ src/libs/app/slashCommandPusher.ts
@@ -4,6 +4,7 @@ const rest = new REST({version: '10'}).setToken(<string>process.env.TOKEN);
 export default class SlashCommandPusher {
     run(client_id: string) {
         return new Promise(async (resolve, reject) => {
+
             await this.push_client_commands(<SlashCommandBuilder[]>slash_commands.get("client"), client_id)
             await this.push_guild_commands(client_id)
         })
@@ -26,7 +27,6 @@ export default class SlashCommandPusher {
         slash_commands.each(async (value, key) => {
             if (key == "client") return;
             try {
-                console.log('[SlashCommandPusher] Initializing guild (' + key +') commands.');
 
                 await rest.put(Routes.applicationGuildCommands(client_id, key), {
                     body: value.map(val => val.toJSON())
